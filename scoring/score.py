a1de6700b38696c625dea240a71a5775104a2981
Alistair Lynn <arplynn@gmail.com>
2016-07-25 15:47:12 +0100

    Add scorer script

---
+++ scoring/score.py
@@ -0,0 +1,17 @@
+class Scorer:
+    def __init__(self, scoresheet):
+        self.scoresheet = scoresheet
+
+    def calculate_score(self, data):
+        moved = data.get('moved', False)
+        poison = data.get('poison', False)
+        num_tokens = data.get('tokens', 0)
+        token_score = 0 if poison else num_tokens
+        movement_score = 1 if moved else 0
+        return token_score + movement_score
+
+    def calculate_scores(self):
+        return {
+            tla: self.calculate_score(data)
+            for tla, data in self.scoresheet.items()
+        }
