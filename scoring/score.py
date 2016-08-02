76ee5164765e3cc97c77e1f8fdcf7b440a9211c3
Alistair Lynn <arplynn@gmail.com>
2016-08-02 13:09:42 +0100

    Up the movement points to 2

--- scoring/score.py
+++ scoring/score.py
@@ -7,7 +7,7 @@ class Scorer:
         poison = data.get('poison', False)
         num_tokens = data.get('tokens', 0)
         token_score = 0 if poison else num_tokens
-        movement_score = 1 if moved else 0
+        movement_score = 2 if moved else 0
         return token_score + movement_score
 
     def calculate_scores(self):
